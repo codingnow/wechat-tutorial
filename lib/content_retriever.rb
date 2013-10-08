9ee60d2745d890bbc80e4686170d5860e50b1624
codingnow <carl.caiqh@gmail.com>
2013-10-08 17:06:16 +0800

    Update content_retriever.rb

--- lib/content_retriever.rb
+++ lib/content_retriever.rb
@@ -8,7 +8,7 @@ class ContentRetriver
 			contents: [{
 				title: '简单几步成为微信公众平台开发者',
 				description: '该公众账户是为了演示如何成为微信平台的开发者，如果想要了解更详细的内容，请点击该消息。',
-				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=3097029212&r=975188&token=1685872554',
+				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=2390010006&r=172640&token=1003462586',
 				url: 'http://linkedme.in/2013/09/25/turoial-to-be-wechat-public-account-developer/'}]
 		}
 	end
